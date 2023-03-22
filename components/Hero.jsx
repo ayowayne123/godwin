77f479a860640fdfde4f7dd6d182567620b94761
ayowayne123 <ayowayne123@gmail.com>
2023-03-22 16:40:46 +0200

    nav colour

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,30 +1,28 @@
 'use client'
-import React, { useState, useEffect } from 'react';
+import React, { useState } from "react";
 import Image from 'next/image'
 import hero from '../public/godwinWhite.png'
+import Logo from '../public/logo.png'
 import { Link } from 'react-scroll'
 
 
 function Hero() {
-  const [prevScrollPos, setPrevScrollPos] = useState(0);
-  const [visible, setVisible] = useState(true);
-  const [transparent,setTransparent] = useState(true);
+  
+  
+  const [colorChange, setColorchange] = useState(false);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const currentScrollPos = window.scrollY;
-      setVisible((prevScrollPos > currentScrollPos && prevScrollPos - currentScrollPos > 0.1) || currentScrollPos < 10);
-      console.log('scroll',prevScrollPos, currentScrollPos,visible);
-      setPrevScrollPos(currentScrollPos);
-    };
+  const changeNavbarColor = () => {
+    if (window.scrollY >= 80) {
+      setColorchange(true);
+    } else {
+      setColorchange(false);
+    }
+  };
+  if (typeof window !== "undefined") {
+    window.addEventListener("scroll", changeNavbarColor);
+  }
+   
 
-    if (window.scrollY>40)
-    setTransparent(false)
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
   
   return (
     <div className='grid grid-cols-2 h-[100dvh] w-full overflow-hidden'>
@@ -38,9 +36,13 @@ function Hero() {
 </div>
 <div className='relative w-full h-full' > 
 <Image src={hero} alt='hero Image' className='object-cover bg-[#DEDEDE]' fill/> </div>
-<div className={` top-0 w-full z-20 ${visible ? 'fixed flex ' : 'hidden'} ${transparent? 'bg-transparent' : 'bg-white'} `}>
-    <div className='grid grid-cols-3 w-full '> 
-    <div></div>
+<div className={` top-0 w-full z-20 fixed flex ${colorChange ? "bg-white" : "bg-inherit"} `}>
+    <div className='grid grid-cols-3 w-full container '> 
+    <div className="h-16 col-span-1 w-full inline-flex items-center justify-self=start  "> 
+    <div className="relative h-10 w-full items-center justify-start flex  ">
+      <Image src={Logo} alt='hero Image' className='object-contain  w-fit h-8 ' /></div>
+    </div>
+    
     <div className='flex flex-row col-span-2 justify-around h-16 items-center font-bold text-[#1E1E1E] text-lg'><Link className='cursor-pointer' smooth={true}  to='music'>Music</Link>
     <Link className='cursor-pointer' smooth={true} to='videos'>Videos</Link>
     <Link className='cursor-pointer' smooth={true} to='#events'>Events</Link>
